660993db3091a88af9addfcf1ce133422315f823
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-04-13 13:01:49 -0400

    eslint airbnb insatlled and working

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { withRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
 function App() {
-  return (
+    return (
 
-    <div>
-    <Route
-        exact
-        path='/'
-        render={(props) => (
-            <p>
-            HomePage
-            </p>
-        )}
-    />
-    <Route
-        exact
-        path='/page1'
-        render={(props) => (
-            <p>
-            Page 1
-            </p>
-        )}
-    />
-    <Route
-        exact
-        path='/page2'
-        render={(props) => (
-            <p>
-            Page 2
-            </p>
-        )}
-    />
-    </div>
+        <div>
+            <Route
+                exact
+                path='/'
+                render={(props) => (
+                    <p>
+                        HomePage
+                    </p>
+                )}
+            />
+            <Route
+                exact
+                path='/page1'
+                render={(props) => (
+                    <p>
+                        Page 1
+                    </p>
+                )}
+            />
+            <Route
+                exact
+                path='/page2'
+                render={(props) => (
+                    <p>
+                        Page 2
+                    </p>
+                )}
+            />
+        </div>
 
-  );
+    );
 }
 
 export default App;
