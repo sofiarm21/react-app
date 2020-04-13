ed7e06a784a4e14a8f69112c9ceaac5ea8920cd1
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-04-13 12:21:43 -0400

    react route configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import './App.css';
+import { withRouter, Route } from 'react-router-dom';
+
 
 function App() {
   return (
+
     <div>
+    <Route
+        exact
+        path='/'
+        render={(props) => (
+            <p>
+            HomePage
+            </p>
+        )}
+    />
+    <Route
+        exact
+        path='/page1'
+        render={(props) => (
+            <p>
+            Page 1
+            </p>
+        )}
+    />
+    <Route
+        exact
+        path='/page2'
+        render={(props) => (
+            <p>
+            Page 2
+            </p>
+        )}
+    />
     </div>
+
   );
 }
 
